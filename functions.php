c97187f5bd7fae448c66c3106c5679352542b84c
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-11-21 00:29:15 +0100

    Clean up theme functions

--- functions.php
+++ functions.php
@@ -1,32 +1,74 @@
 <?php
+/** Little Blue Bag function theme function definitions */
 
+/**
+ * Add support for bootstraps navigation style
+ */
 require(get_template_directory().'/inc/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php');
+
+/**
+ * Setup theme options
+ */
+add_action( 'after_setup_theme', function () {
+
+    /**
+     * local fix since curl isn't working properly
+     * on yosemite
+     */
+    WP_DEBUG && add_filter( 'http_api_transports', function () {
+        return array( 'streams' );
+    });
+
+    /**
+     * i18n text support
+     *
+     * Translations can be added to the /languages/ directory.
+     */
+    load_theme_textdomain( 'littlebluebag', get_template_directory() . '/languages' );
+
+    // Add RSS feed links to <head> for posts and comments.
+    add_theme_support('automatic-feed-links');
+
+    // Enable support for Post Thumbnails for image wall
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 400, 400 );
+    add_image_size("image-wall", 1140, 760, true);
+
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support('html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+    ));
+});
 
 /**
  * Enqueue scripts and styles
  */
-function greyskull14_scripts() {
+add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() .'/static/css/bootstrap.css');
     wp_enqueue_style( 'greyskull_css', get_template_directory_uri() .'/static/css/style.css');
 
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
     wp_enqueue_script('bootstrap', get_template_directory_uri() .'/static/js/bootstrap.js');
     wp_enqueue_script('modernizr', get_template_directory_uri() .'/static/js/modernizr.js');
-}
 
-add_action( 'wp_enqueue_scripts', 'greyskull14_scripts' );
-
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+});
 
 /**
  * header register menus
  */
-function greyskull_register_menu() {
+add_action( 'init', function () {
     register_nav_menu('header-menu',__( 'Header Menu' ));
     register_nav_menu('header-right-menu', __('Right Header Menu'));
-}
-add_action( 'init', 'greyskull_register_menu' );
+});
 
-function greyskull_widgets_init() {
+
+add_action( 'widgets_init', function () {
 
     register_sidebar( array(
         'name'          => __( 'Sidebar Widget Area', 'greyskull14' ),
@@ -47,45 +89,17 @@ function greyskull_widgets_init() {
         'before_title'  => '<h1>',
         'after_title'   => '</h1><hr/>',
     ) );
-
-
-}
-add_action( 'widgets_init', 'greyskull_widgets_init' );
-
-add_theme_support('automatic-feed-links');
-add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size( 400, 400 );
-
-add_image_size("image-wall", 1140, 760, true);
-
-/*
- * Switch default core markup for search form, comment form, and comments
- * to output valid HTML5.
- */
-add_theme_support( 'html5', array(
-    'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-) );
-
-
-if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    wp_enqueue_script( 'comment-reply' );
-}
-
+});
 
 // Replaces the excerpt "more" text by a link
-function new_excerpt_more($more) {
+add_filter('excerpt_more', function ($more) {
     global $post;
 
     return
         '<p><a class="moretag" href="'. get_permalink($post->ID) . '">'
         .__( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' )
         .'</a></p>';
-}
-
-add_filter('excerpt_more', 'new_excerpt_more');
-
-// Add all users to author
-// Filter to fix the Post Author Dropdown
+});
 
 /**
  * Override post author drop down to also include
@@ -94,7 +108,7 @@ add_filter('excerpt_more', 'new_excerpt_more');
  * @param $output
  * @return mixed|null|string
  */
-function theme_post_author_override($output) {
+add_filter('wp_dropdown_users', function ($output) {
 
     /**
      * Get the global post data to make sure
@@ -120,9 +134,11 @@ function theme_post_author_override($output) {
     $output = preg_replace('/post_author_override_replaced/', 'post_author_override', $output);
 
     return $output;
-}
-add_filter('wp_dropdown_users', 'theme_post_author_override');
+});
 
+/**
+ * Entry pager function
+ */
 function grayskull_numeric_posts_nav() {
 
     if( is_singular() )
@@ -189,17 +205,6 @@ function grayskull_numeric_posts_nav() {
         printf( '<li>%s</li>' . "\n", get_next_posts_link() );
 
     echo '</ul>' . "\n";
-
 }
 
 
-load_theme_textdomain( 'littlebluebag', get_template_directory() . '/languages' );
-
-
-/**
- * local fix since curl isn't working properly
- * on yosemite
- */
-add_filter( 'http_api_transports', function() {
-    return array( 'streams' );
-});
