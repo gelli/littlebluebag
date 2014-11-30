88ba7a5da8268d87264f893d2d0f5b656ee5a3c5
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-11-30 21:35:54 +0100

    Remove scaling effect from images and replace by a darkening on hover
    The scaling made the picture and especially text look really bad in chrome.

--- frontpost.php
+++ frontpost.php
@@ -9,10 +9,16 @@
  * @since Twenty Fourteen 1.0
  */
 ?>
-    <div class="col-md-6 col-xs-12">
-        <a class="img-wrap" href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail("image-wall", array('class' => 'img-responsive')); ?>
-            <div><!-- <p><?php echo get_the_title(); ?> </p>--> </div>
-        </a>
-    </div>
+<div class="col-md-6 col-xs-12">
+  <div class="lbb-feature">
+      <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail("image-wall", array('class' => 'img-responsive lbb-feature-image')); ?>
+      </a>
+
+    <?php /*
+    <div class="lbb-feature-meta">
+      <h2><?php echo get_the_title(); ?></h2>
+    </div> */ ?>
+  </div>
+</div>
 
