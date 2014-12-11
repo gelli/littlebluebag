0a1e7199e19349b789d8b74efc52165f1a25bf14
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-12-11 23:25:50 +0100

    Refactor to add newsletter signup banner

--- front-page.php
+++ front-page.php
@@ -14,61 +14,71 @@
 get_header(); ?>
     <div id="main-content" class="main-content">
         <div class="row sam-content">
-            <div class="col-md-4">
-                <div class="welcome text-center">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img id="img-suitcase" src="<?php echo get_template_directory_uri()?>/static/images/suitcase.png"/></a>
-                    <h1 style="font-size:3.6em;" class="sam-color-two">Welcome</h1>
-                    <h4 class="sam-color-grey">and thank you for visiting</h4>
-                    <img id="littleblue-slogan" src="<?php echo get_template_directory_uri()?>/static/images/bluebag_logo_solo.png"/>
-                    <h2 class="sam-color-one">Katrin <span class="sam-color-two">&</span> Sandra</h2>
-                    <hr/>
-                    <div>
-                        <a href="https://www.facebook.com/littlebluebag" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf101;</a>
-                        <a href="https://plus.google.com/112711029813738652601/posts" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf102;</a>
-                        <a href="http://www.pinterest.com/katrinandsandra/" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf106;</a>
-                        <a href="https://vimeo.com/katrinandsandra" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf109;</a>
-                        <a href="https://www.youtube.com/channel/UCeqThqTs9zDDQVvlDIbACxg" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf10a;</a>
-                    </div>
-                    <hr/>
-                        <!-- <a href="https://www.globetrotter.de/?utm_source=PTID13002690"><img src="http://ad.globetrotter-partnerprogramm.de/1-globetrotter-300x250.gif" alt="Globetrotter" /></a> -->
-                    <a href="ebook-manuell-fotografieren">
-                        <img src="<?php echo get_template_directory_uri()?>/static/images/ebook.png" border="1"/>
+            <div class="col-md-4 ">
+                <div class="row">
+                    <div class="welcome text-center col-xs-12">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img id="img-suitcase" src="<?php echo get_template_directory_uri()?>/static/images/suitcase.png"/></a>
+                        <h1 style="font-size:3.6em;" class="sam-color-two">Welcome</h1>
+                        <h4 class="sam-color-grey">and thank you for visiting</h4>
+                        <img id="littleblue-slogan" src="<?php echo get_template_directory_uri()?>/static/images/bluebag_logo_solo.png"/>
+                        <h2 class="sam-color-one">Katrin <span class="sam-color-two">&</span> Sandra</h2>
+                        <hr/>
+                        <div>
+                            <a href="https://www.facebook.com/littlebluebag" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf101;</a>
+                            <a href="https://plus.google.com/112711029813738652601/posts" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf102;</a>
+                            <a href="http://www.pinterest.com/katrinandsandra/" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf106;</a>
+                            <a href="https://vimeo.com/katrinandsandra" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf109;</a>
+                            <a href="https://www.youtube.com/channel/UCeqThqTs9zDDQVvlDIbACxg" target="_blank" class="sam-icon-size-48 sam-icon sam-color-three">&#xf10a;</a>
+                        </div>
+                        <hr/>
+                            <!-- <a href="https://www.globetrotter.de/?utm_source=PTID13002690"><img src="http://ad.globetrotter-partnerprogramm.de/1-globetrotter-300x250.gif" alt="Globetrotter" /></a> -->
+                        <a href="ebook-manuell-fotografieren">
+                            <img src="<?php echo get_template_directory_uri()?>/static/images/ebook.png" border="1"/>
 
-                    </a>
-                    <hr/>
-                    <div class="teaser-box text-left">
-                        <p>
-                            <a href="/about/"><span class="teaser-camera glyphicon glyphicon-camera"></span></a> Katrin und Sandra.
-                        </p><p>
-                            Auf jeden Fall zu zweit unterwegs und gemeinsam wollen wir eins: In Bildern Geschichten erzählen.
-                        </p>
-                        <p>
-                            Persönlich. Lebendig. Einzigartig. Wir helfen euch aus eurem Urlaub ein unvergessliches Abenteuer werden zu lassen.
-                        </p>
-                        <p>Wir geben euch Tipps rund um die Urlaubsplanung, die Reisefotografie und das Filmen unterwegs. </p>
-                        <p>Auch die Bildverwaltung, die Bearbeitung und der Filmschnitt werden Thema dieses Reiseblogs sein.</p>
-                        <p>Es ist euer Abenteuer. Entspannt euch! Wir sind für euch da.</p>
-                        <p class="text-right">Katrin und Sandra.</p>
+                        </a>
+                        <hr/>
                     </div>
 
-                    <div class="teaser-box sam-boxed">
-                        <p>
-                            <a target="_blank" href="http://www.instagram.com/katrinandsandra" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
-                            <a target="_blank" href="http://www.instagram.com/katrinandsandra">Folge <strong>Katrin</strong> auf Instagram</a>
-                        </p>
-                        <p>
-                            <a target="_blank" href="http://www.instagram.com/littlebluebag" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
-                            <a target="_blank" href="http://www.instagram.com/littlebluebag">Folge <strong>Sandra</strong> auf Instagram</a>
-                        </p>
+                    <div class="text-center col-xs-12">
+                        <div class="teaser-box text-left">
+                            <p>
+                                <a href="/about/"><span class="teaser-camera glyphicon glyphicon-camera"></span></a> Katrin und Sandra.
+                            </p><p>
+                                Auf jeden Fall zu zweit unterwegs und gemeinsam wollen wir eins: In Bildern Geschichten erzählen.
+                            </p>
+                            <p>
+                                Persönlich. Lebendig. Einzigartig. Wir helfen euch aus eurem Urlaub ein unvergessliches Abenteuer werden zu lassen.
+                            </p>
+                            <p>Wir geben euch Tipps rund um die Urlaubsplanung, die Reisefotografie und das Filmen unterwegs. </p>
+                            <p>Auch die Bildverwaltung, die Bearbeitung und der Filmschnitt werden Thema dieses Reiseblogs sein.</p>
+                            <p>Es ist euer Abenteuer. Entspannt euch! Wir sind für euch da.</p>
+                            <p class="text-right">Katrin und Sandra.</p>
+                        </div>
                     </div>
 
-                    <div class="teaser-box teaser-img">
-                        <a href="/about/">
-                            <?php echo wp_get_attachment_image( 267, 'post-thumbnail', false, array('class' => 'img-responsive')); ?>
-                         </a>
+                    <div class="col-xs-12  col-sm-6 col-md-12">
+                        <div class="teaser-box sam-boxed text-center">
+                            <p>
+                                <a target="_blank" href="http://www.instagram.com/katrinandsandra" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
+                                <a target="_blank" href="http://www.instagram.com/katrinandsandra">Folge <strong>Katrin</strong> auf Instagram</a>
+                            </p>
+                            <p>
+                                <a target="_blank" href="http://www.instagram.com/littlebluebag" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
+                                <a target="_blank" href="http://www.instagram.com/littlebluebag">Folge <strong>Sandra</strong> auf Instagram</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-6 col-md-12">
+                        <div class="teaser-box">
+                            <a href="/about/">
+                                <?php echo wp_get_attachment_image( 267, 'post-thumbnail', false, array('class' => 'img-responsive teaser-img')); ?>
+                             </a>
+                        </div>
                     </div>
                 </div>
             </div> <!-- col-md-4 -->
+
             <div class="col-md-8">
                 <div class="row image-wall">
                     <?php
@@ -109,38 +119,62 @@ get_header(); ?>
             </div>
         </div>
 
-        <!-- Favorite Articles -->
-        <?php
-            $query = new WP_Query( array (
-                'post_type' => 'post',
-                'posts_per_page' => '3',
-                'category_name' => 'featured'
-            ));
 
-            if ( $query->have_posts() ) : ?>
+        <!-- Custom Category One -->
+        <?php
+        $query = new WP_Query( array (
+          'post_type' => 'post',
+          'posts_per_page' => '3',
+          'category_name' => 'featured'
+        ));
+
+        if ( $query->have_posts() ) : ?>
             <div class="row">
                 <h2 id="featured-posts"><?php _e( 'Most Popular Articles', 'littlebluebag' ); ?></h2>
 
                 <?php //// Start the Loop.
                 while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                    <div class="col-md-4 col-xs-12 sam-featured">
+                    <div class="col-md-4 col-sm-4 col-xs-12 sam-featured">
                         <a class="img-wrap" href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail("image-wall", array('class' => 'img-responsive')); ?>
+                            <?php the_post_thumbnail("image-wall", array('class' => 'img-responsive lbb-feature-image')); ?>
                             <div><!-- <p><?php echo get_the_title(); ?> </p>--> </div>
                         </a>
                     </div>
 
                 <?php endwhile; ?>
             </div>
-            <?php else :
-                // If no content, include the "No posts found" template.
-                get_template_part( 'content', 'none' );
+        <?php endif; ?>
 
-            endif;
-        ?>
+        <div class="row space-top">
+            <div class="col-xs-12">
+                <div class="panel panel-default panel-newsletter">
+                    <div class="panel-body  ">
+                        <h3>Newsletter</h3>
+                        <hr class="visible-sm visible-md visible-lg"/>
+                        <p class="visible-md visible-lg">
+                            Ihr wollt immer von den neuesten Neuigkeiten erfahren? Kein Problem!
+                            Meldet euch für unseren monatlichen Newsletter an.
+                        </p>
+                        <form method="post" action="/wp-content/plugins/newsletter/do/subscribe.php" onsubmit="return newsletter_check(this)">
+                            <p>
+                            <div class="input-group">
+                                <span class="input-group-addon">@</span>
+                                <input type="email" name="ne" class="form-control" placeholder="E-Mail">
+                                <span class="input-group-btn">
+                                    <input class="newsletter-submit btn btn-default" type="submit" value="Anmelden"/>
+                                </span>
+                            </div>
 
-        <div class="clearfix" style="margin-top:20px;"></div>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="clearfix space-top"></div>
     </div>
 <?php
 get_footer();
