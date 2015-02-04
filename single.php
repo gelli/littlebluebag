6924a5439b80980e0286fec5f1e20de874498bdd
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-02-04 22:08:36 +0100

    Show newsletter signup below each article

--- single.php
+++ single.php
@@ -27,6 +27,39 @@ get_header(); ?>
                             // Previous/next post navigation.
                             /* twentyfourteen_post_nav(); */
 
+                            ?>
+
+                            <div class="row space-top">
+                                <div class="col-xs-12">
+                                    <div class="panel panel-default panel-newsletter">
+                                        <div class="panel-body  ">
+                                            <h3>Newsletter</h3>
+                                            <hr class="visible-sm visible-md visible-lg"/>
+                                            <p class="visible-md visible-lg">
+                                                Ihr wollt immer von den neuesten Neuigkeiten erfahren? Kein Problem!
+                                                Meldet euch für unseren monatlichen Newsletter an.
+                                            </p>
+                                            <form method="post" action="/wp-content/plugins/newsletter/do/subscribe.php" onsubmit="return newsletter_check(this)">
+                                                <p>
+                                                <div class="input-group">
+                                                    <span class="input-group-addon">@</span>
+                                                    <input type="email" name="ne" class="form-control" placeholder="E-Mail">
+                                <span class="input-group-btn">
+                                    <input class="newsletter-submit btn btn-default" type="submit" value="Anmelden"/>
+                                </span>
+                                                </div>
+
+                                                </p>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <?php
+
+
                             // If comments are open or we have at least one comment, load up the comment template.
                             if ( comments_open() || get_comments_number() ) {
                                 comments_template();
@@ -35,6 +68,10 @@ get_header(); ?>
                     ?>
                 </div><!-- #content -->
             </div><!-- #primary -->
+
+
+
+
         </div> <!-- .col -->
         <div class="col-md-3">
             <?php get_sidebar( 'sam-sidebar') ?>
