6bb3c9e8f4445cbe385f285725f47473a5709dd3
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-03-16 23:36:02 +0100

    Add category and date Add icons for post meta data

--- content.php
+++ content.php
@@ -19,7 +19,8 @@
             <div class="day"><?php echo get_the_date('d'); ?></div>
         </div>
 
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) : ?>
+
+    <?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) : ?>
 		<?php
 			endif;
 
@@ -36,17 +37,30 @@
 		?>
 
         <div class="entry-meta clearfix">
+
+          <?php
+          // Set up and print post meta information.
+          printf( '<span class="entry-date"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span> <a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s">%3$s</time></a></span>',
+          esc_url( get_permalink() ),
+          esc_attr( get_the_date( 'c' ) ),
+          esc_html( get_the_date() ),
+          esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
+          );
+          ?>
+
+          <span class="cat-links"><span class="glyphicon glyphicon-tag" aria-hidden="true"></span> <?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+
             <?php
             if ( 'post' == get_post_type() )
                 /* twentyfourteen_posted_on(); */
 
                 if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
                     ?>
-                    <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
+                    &nbsp;<span class="comments-link"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
                 <?php
                 endif;
 
-            edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
+            edit_post_link( __( 'Edit', 'twentyfourteen' ), '&nbsp;<span class="edit-link"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> ', '</span>' );
             ?>
         </div><!-- .entry-meta -->
 
